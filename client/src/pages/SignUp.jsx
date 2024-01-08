0cf645520b3ae55e6862c9597785ada11fbfcf17
codewithSahil07 <codewithsahil.dev@gmail.com>
2024-01-08 20:54:07 +0530

    added funtionalities to signup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,18 +1,47 @@
 import React, { useState } from "react";
-import { Link }from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
-  const [data, setData] = useState({});
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
   const inputHandler = (e) => {
-    setData({
-      ...data,
-      [e.target.id] : e.target.value,
-    })
-  }
-  const submitHandler = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const submitHandler = async (e) => {
     e.preventDefault();
-  }
-  console.log(data);
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if(data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/sign-in');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+      // console.log(error);
+    }
+  };
+
+  console.log(formData);
   return (
     <div className="my-5 p-5 max-w-lg m-auto">
       <div className="font-semibold text-center text-3xl">Sign Up</div>
@@ -38,15 +67,23 @@ export default function SignUp() {
           id="password"
           className="my-2 p-3 border rounded-lg"
         />
-        <button className="my-2 p-2 rounded-lg bg-slate-600 text-white uppercase hover:opacity-90 disabled:opacity-70">Sign Up</button>
-        <button className="my-2 p-2 rounded-lg bg-slate-600 text-white uppercase hover:opacity-90 disabled:opacity-70">Sign Up with Google</button>
+        <button
+          // disabled={loading}
+          className="my-2 p-2 rounded-lg bg-slate-600 text-white uppercase hover:opacity-90 disabled:opacity-70"
+        >
+          {loading ? "loading......" : "Sign Up"}
+        </button>
+        <button className="my-2 p-2 rounded-lg bg-slate-600 text-white uppercase hover:opacity-90 disabled:opacity-70">
+          Sign Up with Google
+        </button>
       </form>
-      <div className="flex gap-3 justify-center">
+      <div className="flex gap-2 justify-center">
         <p>Already have an account?</p>
         <Link to={"/sign-in"}>
           <span className="text-blue-700">Sign in</span>
         </Link>
       </div>
+      {error && <p>{error}</p>}
     </div>
   );
 }
