f49f2eaa17221e0e3bed16f4f26affbb04fa4bf9
codewithSahil07 <codewithsahil.dev@gmail.com>
2024-01-08 23:21:08 +0530

    created signin route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,16 +1,38 @@
 import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs';
+import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utills/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
-    const { username, email, password } = req.body;
-    const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({username, email ,  password : hashedPassword});
-    try {
-        await newUser.save();
-        res.status(201).json("User Created Successfully");
-    } catch (error) {
-        next(error);
-        // next(errorHandler(501, "Test Error"));
-    }
-}
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json("User Created Successfully");
+  } catch (error) {
+    next(error);
+    // next(errorHandler(501, "Test Error"));
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(401, "Invalid Email"));
+    const validPassword = bcryptjs.compareSync(
+      password,
+      validUser.password
+    );
+    if (!validPassword) return next(errorHandler(401, "Invalid Password"));
+    const { password: pass, ...rest } = validUser._doc;
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY);
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
