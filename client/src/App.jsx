dcbc44b4d057427d22c47f65260ad02d696d66e8
codewithSahil07 <codewithsahil.dev@gmail.com>
2024-01-11 23:51:38 +0530

    add profile pin in header and make profile route private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,32 @@
 import React from "react";
-import './index.css';
-import { BrowserRouter , Routes , Route } from 'react-router-dom';
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import About from "./pages/About";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
-  return <div>
-    <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path="/sign-up" element={<SignUp/>}/>
-      <Route path="/sign-in" element={<SignIn/>}/>
-      <Route path="/home" element={<Home/>}/>
-      <Route path="/profile" element={<Profile/>}/>
-      <Route path="/about" element={<About/>}/>
-    </Routes>
-    </BrowserRouter>
-  </div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/home" element={<Home />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,24 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Home from "../pages/Home";
+import { useSelector } from "react-redux";
 
 export default function Header() {
-  return <header className="bg-slate-200">
-    <div className="flex justify-between p-3 items-center">
-    <Link to="/">
-        <h1 className="font-bold flex text-md sm:text-lg">
+  const {currentUser} = useSelector((state) => state.user);
+  console.log(currentUser);
+
+  return (
+    <header className="bg-slate-200">
+      <div className="flex justify-between p-3 items-center">
+        <Link to="/">
+          <h1 className="font-bold flex text-md sm:text-lg">
             <span className="text-slate-800">Sahil</span>
             <span className="text-slate-500">Estate</span>
-        </h1>
-    </Link>
-    <form className="bg-slate-100 rounded-lg p-3 flex items">
-        <input type="text" className="bg-transparent focus:outline-none" placeholder="Search . . . ." />
-    </form>
-    <ul className="flex gap-3">
-        <Link to="/"><li className="hidden sm:inline hover:underline">Home</li></Link>
-        <Link to="/about"><li className="hidden sm:inline hover:underline">about</li></Link>
-        <Link to="/sign-in"><li className="hover:underline">Logout</li></Link>
-    </ul>
-    </div>
-  </header>;
+          </h1>
+        </Link>
+        <form className="bg-slate-100 rounded-lg p-3 flex items">
+          <input
+            type="text"
+            className="bg-transparent focus:outline-none"
+            placeholder="Search . . . ."
+          />
+        </form>
+        <ul className="flex gap-3">
+          <Link to="/">
+            <li className="hidden sm:inline hover:underline">Home</li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline hover:underline">about</li>
+          </Link>
+          <Link to="/profile">
+            {currentUser ? (
+              <img className="w-8 h-8 rounded-full object-cover" src={currentUser.avatar} alt='Profile-Pic'/>
+            ) : (
+              <li className="hover:underline">Signin</li>
+            )}
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
 }
