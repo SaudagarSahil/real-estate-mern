61a1e931f0bbf5f120f3bdefe668bad7828a04c7
codewithSahil07 <codewithsahil.dev@gmail.com>
2024-05-09 14:39:38 +0530

    add api for getting listings of a user

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { errorHandler } from "../utills/error.js";
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js";
 
 export const test = (req, res) => {
   res.send("Hello Sahil");
@@ -47,3 +48,18 @@ export const deleteUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getUserListing = async (req, res, next) => {
+  if(req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({userRef : req.params.id});
+      res.status(201).json(listings);
+      console.log("Listings retrived !")
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, 'You can only view your listings :)'));
+  }
+
+}
