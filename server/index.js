9cd3e720cb39bce0a329486599483a566202426f
Sahil Saudagar <codewithsahil.dev@gmail.com>
2025-01-23 22:54:30 +0530

    make changes in package.json

--- server/index.js
+++ server/index.js
@@ -5,23 +5,26 @@ import dotenv from 'dotenv';
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
+import path from 'path';
 dotenv.config();
+
+// console.log("MONGODB STR",process.env.MONGO);
+
+mongoose
+.connect(process.env.MONGO)
+.then((conn) => {
+  console.log("Connected to MongoDB", conn.connection.name);
+})
+.catch((err) => {
+  console.log(err);
+});
+
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
 
 app.use(cookieParser());
-
-// console.log("MONGODB STR",process.env.MONGO);
-
-mongoose
-  .connect(process.env.MONGO)
-  .then((conn) => {
-    console.log("Connected to MongoDB", conn.connection.name);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
@@ -30,6 +33,12 @@ app.listen(3000, () => {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
